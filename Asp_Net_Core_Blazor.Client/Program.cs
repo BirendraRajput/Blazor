fcd7fd1f361614be566a9918966958296f17655f
Birendra Rajput <birendra.singhrjput@freds.com>
2019-06-21 14:09:52 +0100

    Initial Checkin
    Created new repository for Blazor Hello World Application

---
+++ Asp_Net_Core_Blazor.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Hosting;
+
+namespace Asp_Net_Core_Blazor.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
